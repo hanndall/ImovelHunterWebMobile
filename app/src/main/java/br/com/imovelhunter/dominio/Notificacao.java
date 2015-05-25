2b8c049fc5f330b5cd61373a7fc1d21fd39d0766
rodrigox <rodrigoalex23@hotmail.com>
2015-05-25 12:08:48 -0300

    Alteraçoes da classe de notificacao

--- app/src/main/java/br/com/imovelhunter/dominio/Notificacao.java
+++ app/src/main/java/br/com/imovelhunter/dominio/Notificacao.java
@@ -7,11 +7,93 @@ import br.com.imovelhunter.util.ObjetoJSON;
 
 public class Notificacao extends ObjetoJSON<Notificacao> implements Serializable{
 
-    private long idNotificacao;
+    private Long idNotificacao;
+
+    private String rua;
+
+    private String numero;
+
+    private String caminhoImagem;
+
+    private double preco;
+
+    private String tipo;
+
+    private String situacao;
+
+    private double latitude;
+
+    private double longitude;
 
     private String mensagemNotificacao;
-    
+
     private Object tag;
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getCaminhoImagem() {
+        return caminhoImagem;
+    }
+
+    public void setCaminhoImagem(String caminhoImagem) {
+        this.caminhoImagem = caminhoImagem;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getSituacao() {
+        return situacao;
+    }
+
+    public void setSituacao(String situacao) {
+        this.situacao = situacao;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+
 
 
     public long getIdNotificacao() {
